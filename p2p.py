91579feedd78355d9646ebcb788710774fd24f42
Trevor Foland <trevorfoland@outlook.com>
2021-03-26 12:08:28 -0400

    Working on figuring out tkinter

--- p2p.py
+++ p2p.py
@@ -1,6 +1,24 @@
 from _thread import *
 import threading
 from socket import *
+import tkinter as tk
+
+
+
+class GUI:
+    def __init__(self):
+
+        self.window = tk.Tk()
+        self.window.withdraw()
+
+        self.login = tk.Toplevel()
+        self.login.title("Login")
+        self.login.resizable(width = False, height = False)
+        self.login.configure(width = 500, height = 500)
+        self.name_label = tk.Label(self.login, text = "Name: ", font = "Helvetica 16")
+        self.name_label.place(relheight = 0.3, relx = 0.4, rely = 0.2)
+        self.window.mainloop()
+
 
 def client_func():
     client_socket = socket(AF_INET, SOCK_STREAM)
@@ -36,6 +54,7 @@ def srvr_thread(sckt):
     msg = sckt.recv(1024).decode()
     print("Bro: " + msg)
 
+window = GUI()
 start_new_thread(client_func, ())
 start_new_thread(srvr_func, ())
 
