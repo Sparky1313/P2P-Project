83dd23fd7e9f83347a6fe9ea02d393c27b43f5a8
Trevor Foland <trevorfoland@outlook.com>
2021-04-17 11:05:45 -0400

    Developed GUI more

--- p2p.py
+++ p2p.py
@@ -32,34 +32,60 @@ class App:
         self.app = QApplication([])
 
 
-        # Host selection components and layout
+        # My selection components and layout
+        # Components
+        self.my_port_input = QLineEdit("Enter your port to run on...")
+        self.my_port_input_lbl = QLabel("Enter your port number:")
+        self.sign_in_btn = QPushButton("Sign In")
+
+        self.my_port_input_lbl.setBuddy(self.my_port_input)
+        # self.sign_in_btn.clicked.connect(self.sign_in())
+
+        my_sel_layout = QHBoxLayout()
+        my_sel_layout.addWidget(self.my_port_input_lbl)
+        my_sel_layout.addWidget(self.my_port_input)
+        my_sel_layout.addWidget(self.sign_in_btn)
+
+
+        # Friend selection components and layout
         # Components
         self.friend_ip_input = QLineEdit("Enter friend's IP address to connect to...")
         self.friend_port_input = QLineEdit("Enter friend's port number to connect to...")
-        self.my_ip_input = QLineEdit("Enter your port to run on...")
-
-        # Layout
-        host_selection_layout = QHBoxLayout()
-        host_selection_layout.addWidget(self.friend_ip_input)
-        host_selection_layout.addWidget(self.friend_port_input)
-        host_selection_layout.addWidget(self.my_ip_input)
-
-        
-        # Start and end connection components and layout
-        # Components
+        self.friend_ip_input_lbl = QLabel("Enter friend's IP Address:")
+        self.friend_port_input_lbl = QLabel("Enter friend's port number:")
         self.connect_btn = QPushButton("Connect")
         self.end_btn = QPushButton("End Connection")
 
+        self.friend_ip_input_lbl.setBuddy(self.friend_ip_input)
+        self.friend_port_input_lbl.setBuddy(self.friend_port_input)
+
         # Layout
-        connection_layout = QHBoxLayout()
-        connection_layout.addWidget(self.connect_btn)
-        connection_layout.addWidget(self.end_btn)
+        friend_sel_layout = QHBoxLayout()
+        friend_sel_layout.addWidget(self.friend_ip_input_lbl)
+        friend_sel_layout.addWidget(self.friend_ip_input)
+        friend_sel_layout.addWidget(self.friend_port_input_lbl)
+        friend_sel_layout.addWidget(self.friend_port_input)
+        friend_sel_layout.addWidget(self.connect_btn)
+        friend_sel_layout.addWidget(self.end_btn)
+        
+
+        # Start and end connection components and layout
+        # Components
+        # self.connect_btn = QPushButton("Connect")
+        # self.end_btn = QPushButton("End Connection")
+
+        # Layout
+        # connection_layout = QHBoxLayout()
+        # connection_layout.addWidget(self.connect_btn)
+        # connection_layout.addWidget(self.end_btn)
 
 
         # Message area components and layout
         # Components
         self.msg_display_area = QTextEdit("Message Display")
         self.msg_input = QLineEdit("Message Input")
+
+        self.msg_display_area.setReadOnly(True)
 
         #Layout
         msg_area_layout = QVBoxLayout()
@@ -85,8 +111,9 @@ class App:
 
         # Window layout
         window_layout = QVBoxLayout()
-        window_layout.addLayout(host_selection_layout)
-        window_layout.addLayout(connection_layout)
+        window_layout.addLayout(my_sel_layout)
+        window_layout.addLayout(friend_sel_layout)
+        # window_layout.addLayout(connection_layout)
         window_layout.addLayout(msg_area_layout)
         window_layout.addLayout(emoji_layout)
 
@@ -97,6 +124,22 @@ class App:
         self.window.setWindowTitle("P2P Chat App")
         self.window.show()
 
+    # Widget callbacks
+    # def sign_in(self):
+    #     data = self.my_port_input.text()
+
+    #     try:
+    #         data = int(data)
+    #     except:
+    #         self.msg_display_area.append("ERROR: Port number entered is not an integer.  Port number must be an integer.")
+        
+
+    #     if data >= 1024 and data <= 49151:
+    #         self.msg_display_area.append("Hooray")
+            
+    #     else:
+    #         self.msg_display_area.append("Invalid input for your port number.  Must be between 1024 to 49151")
+        # start running server
 
 
 
